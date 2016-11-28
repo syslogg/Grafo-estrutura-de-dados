71270865fe2b53d5efd387f2efcc17aaa3023984
Hendy Rodrigues F. Silva <mr.syslogg@gmail.com>
2016-11-28 16:14:56 -0300

    Modify Grafo.h

--- Grafo.h
+++ Grafo.h
@@ -10,7 +10,10 @@ int ExisteAresta(Grafo * g, int vertice1, int vertice2);
 
 //Criar grafo
 Grafo * criar();
+
+//Destruir grafo
 void destruir(Grafo * g);
+
 //Incluir vertice
 void IncluirVertice(Grafo * g, int key);
 
@@ -26,7 +29,6 @@ void DeletarAresta(Grafo * g, int vertice1, int vertice2);
 //Caminho minimo com bellman ford
 void CaminhoMinimo(Grafo * g, int vertice1, int vertice2);
 
-//Fim de programa;
 
 
 
